6a292341c562b248a04a847c295422ca8bdf9d48
barrientos59 <71335187+barrientos59@users.noreply.github.com>
2024-05-07 16:11:13 +0200

    PoderIrAtras

--- app/src/main/java/com/example/proyectofinal/MainActivity.java
+++ app/src/main/java/com/example/proyectofinal/MainActivity.java
@@ -5,21 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
-
 public class MainActivity extends AppCompatActivity {
+
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-
-        // Navegar al fragmento MenuIniciarSesionRegistrar
-        navController.navigate(R.id.iniciarSesion);
+        // Inicializar NavController
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
     }
 
-
-
+    @Override
+    public void onBackPressed() {
+        // Verificar si hay un fragmento en la pila de navegación
+        if (navController.getCurrentDestination() != null && navController.getCurrentDestination().getId() == R.id.menuPrincipal) {
+            // Si estamos en el fragmento de inicio de sesión, cerrar la actividad
+            super.onBackPressed();
+        } else {
+            // Si no estamos en el fragmento de inicio de sesión, navegar hacia atrás en la pila de navegación
+            navController.navigateUp();
+        }
+    }
 
 }
