ba4fd849bad01e8ccfbfa30873068ebeffb0a6cf
barrientos59 <71335187+barrientos59@users.noreply.github.com>
2024-05-10 17:19:55 +0200

    Item_jugador

--- app/src/main/java/com/example/proyectofinal/PatidoFragment.java
+++ app/src/main/java/com/example/proyectofinal/PatidoFragment.java
@@ -1,187 +1,145 @@
 package com.example.proyectofinal;
 
-
 import android.os.Bundle;
-import androidx.fragment.app.Fragment;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class PatidoFragment extends Fragment implements View.OnClickListener {
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.RecyclerView;
 
-    private Button btnAsistencia,btnPerdida,
-            btnTap,btnFalta, btnRob,btnReb,
-            btnFT3,btnMT3,btnFT2,btnMT2,
-            btnFTl,btnMTl,btnFinalizar;
-    NavController navController;
+import com.example.proyectofinal.Model.Jugador;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+import java.util.ArrayList;
+
+public class PatidoFragment extends Fragment {
+
+    private Spinner equipoSpinner, equipoSpinner2;
+    private ArrayList<String> nombresEquipos = new ArrayList<>();
+    private ArrayAdapter<String> adapter1, adapter2;
+    private FirebaseFirestore db;
+    private NavController navController;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_patido, container, false);
+        equipoSpinner = rootView.findViewById(R.id.spinner_equipoLocal);
+        equipoSpinner2 = rootView.findViewById(R.id.spinner_equipoVisitante);
+        db = FirebaseFirestore.getInstance();
         navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment);
 
-        btnAsistencia = rootView.findViewById(R.id.btnAsistencia);
-        btnPerdida = rootView.findViewById(R.id.btnPerdida);
-        btnTap = rootView.findViewById(R.id.btnTap);
-        btnFalta = rootView.findViewById(R.id.btnfalta);
-        btnRob = rootView.findViewById(R.id.btnRobo);
-        btnReb = rootView.findViewById(R.id.btnRebote);
-        btnFT3 = rootView.findViewById(R.id.btnFallaT3);
-        btnMT3 = rootView.findViewById(R.id.btnAnotaT3);
-        btnFT2 = rootView.findViewById(R.id.btnFallaT2);
-        btnMT2 = rootView.findViewById(R.id.btnAnotaT2);
-        btnFTl = rootView.findViewById(R.id.btnFallaTl);
-        btnMTl = rootView.findViewById(R.id.btnAnotaTl);
+        // Configuración de adaptadores para los Spinners
+        adapter1 = new ArrayAdapter<>(requireContext(), android.R.layout.simple_spinner_item, nombresEquipos);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        equipoSpinner.setAdapter(adapter1);
 
-        btnFinalizar =rootView.findViewById(R.id.btnFinalizar);
+        adapter2 = new ArrayAdapter<>(requireContext(), android.R.layout.simple_spinner_item, nombresEquipos);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        equipoSpinner2.setAdapter(adapter2);
 
+        // Cargar equipos en los Spinners
+        cargarEquiposEnSpinner();
 
-        // Ocultar botones de asistencia al iniciar el fragmento
-        hideButtons();
-
-        // Configurar clics de botones de jugadores
-        rootView.findViewById(R.id.j1_e1).setOnClickListener(this);
-        rootView.findViewById(R.id.j2_e1).setOnClickListener(this);
-        rootView.findViewById(R.id.j3_e1).setOnClickListener(this);
-        rootView.findViewById(R.id.j4_e1).setOnClickListener(this);
-        rootView.findViewById(R.id.j5_e1).setOnClickListener(this);
-        rootView.findViewById(R.id.j1_e2).setOnClickListener(this);
-        rootView.findViewById(R.id.j2_e2).setOnClickListener(this);
-        rootView.findViewById(R.id.j3_e2).setOnClickListener(this);
-        rootView.findViewById(R.id.j4_e2).setOnClickListener(this);
-        rootView.findViewById(R.id.j5_e2).setOnClickListener(this);
-
-        // Configurar clics de botones de asistencia, perdida, tapon y falta
-        btnAsistencia.setOnClickListener(new View.OnClickListener() {
+        // Configuración de listeners para los Spinners
+        equipoSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                hideButtons();
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                cargarJugadoresDelEquipo(equipoSpinner.getSelectedItem().toString(), R.id.recyclerView_jugadoresLocal);
+                verificarEquiposDiferentes();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // No se necesita hacer nada aquí
             }
         });
 
-        btnPerdida.setOnClickListener(new View.OnClickListener() {
+        equipoSpinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                hideButtons();
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                cargarJugadoresDelEquipo(equipoSpinner2.getSelectedItem().toString(), R.id.recyclerView_jugadoresVisitante);
+                verificarEquiposDiferentes();
             }
-        });
 
-        btnTap.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                hideButtons();
-            }
-        });
-
-        btnFalta.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                hideButtons();
-            }
-        });
-        btnRob.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                hideButtons();
-            }
-        });
-        btnReb.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                hideButtons();
-            }
-        });
-        btnFT3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                hideButtons();
-            }
-        });
-        btnMT3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                hideButtons();
-            }
-        });
-        btnFT2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                hideButtons();
-            }
-        });
-        btnMT2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                hideButtons();
-            }
-        });
-        btnFTl.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                hideButtons();
-            }
-        });
-        btnMTl.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                hideButtons();
-            }
-        });
-        btnFinalizar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                navController.navigateUp();
+            public void onNothingSelected(AdapterView<?> parent) {
+                // No se necesita hacer nada aquí
             }
         });
 
         return rootView;
     }
 
-    @Override
-    public void onClick(View view) {
-        // Mostrar botones de asistencia al hacer clic en un jugador
-        showButtons();
+    private void verificarEquiposDiferentes() {
+        String equipoSeleccionado1 = equipoSpinner.getSelectedItem().toString();
+        String equipoSeleccionado2 = equipoSpinner2.getSelectedItem().toString();
+
+        if (equipoSeleccionado1.equals(equipoSeleccionado2)) {
+            // Si los equipos son iguales, selecciona un equipo diferente en el segundo Spinner
+            int nuevaSeleccion = (equipoSpinner2.getSelectedItemPosition() + 1) % nombresEquipos.size();
+            equipoSpinner2.setSelection(nuevaSeleccion);
+        }
     }
 
-    private void showButtons() {
-        btnAsistencia.setVisibility(View.VISIBLE);
-        btnPerdida.setVisibility(View.VISIBLE);
-        btnTap.setVisibility(View.VISIBLE);
-        btnFalta.setVisibility(View.VISIBLE);
-        btnRob.setVisibility(View.VISIBLE);
-        btnReb.setVisibility(View.VISIBLE);
-        btnFT3.setVisibility(View.VISIBLE);
-        btnMT3.setVisibility(View.VISIBLE);
-        btnFT2.setVisibility(View.VISIBLE);
-        btnMT2.setVisibility(View.VISIBLE);
-        btnFTl.setVisibility(View.VISIBLE);
-        btnMTl.setVisibility(View.VISIBLE);
+    private void cargarEquiposEnSpinner() {
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        String currentUserId = currentUser.getUid();
+
+        db.collection("equipos")
+                .whereEqualTo("idAutor", currentUserId)
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        nombresEquipos.clear(); // Limpiar la lista antes de agregar los nuevos equipos
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            String nombreEquipo = document.getString("nombre");
+                            nombresEquipos.add(nombreEquipo);
+                        }
+                        adapter1.notifyDataSetChanged();
+                        adapter2.notifyDataSetChanged();
+                    } else {
+                        Toast.makeText(requireContext(), "Error al cargar equipos", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
-    private void hideButtons() {
-        btnAsistencia.setVisibility(View.GONE);
-        btnPerdida.setVisibility(View.GONE);
-        btnTap.setVisibility(View.GONE);
-        btnFalta.setVisibility(View.GONE);
-        btnRob.setVisibility(View.GONE);
-        btnReb.setVisibility(View.GONE);
-        btnFT3.setVisibility(View.GONE);
-        btnMT3.setVisibility(View.GONE);
-        btnFT2.setVisibility(View.GONE);
-        btnMT2.setVisibility(View.GONE);
-        btnFTl.setVisibility(View.GONE);
-        btnMTl.setVisibility(View.GONE);
+    // Método para cargar los jugadores del equipo seleccionado desde Firebase y mostrarlos en el RecyclerView
+    // Método para cargar los jugadores del equipo seleccionado desde Firebase y mostrarlos en el RecyclerView
+    private void cargarJugadoresDelEquipo(String nombreEquipo, int recyclerViewId) {
+        db.collection("jugadores")
+                .whereEqualTo("equipo", nombreEquipo)
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        ArrayList<Jugador> jugadores = new ArrayList<>();
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            String nombre = document.getString("nombre");
+                            String apellido = document.getString("apellido");
+                            String dorsal = document.getString("dorsal");
+                            String posicion = document.getString("posicion");
+                            Jugador jugador = new Jugador(nombre, apellido, dorsal, posicion);
+                            jugadores.add(jugador);
+                        }
+                        RecyclerView recyclerView = requireView().findViewById(recyclerViewId);
+                        JugadorAdapter adapter = new JugadorAdapter(jugadores);
+                        recyclerView.setAdapter(adapter);
+                        adapter.notifyDataSetChanged(); // Notificar al RecyclerView que los datos han cambiado
+                    } else {
+                        Toast.makeText(requireContext(), "Error al cargar jugadores del equipo " + nombreEquipo, Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
+
 
 }
